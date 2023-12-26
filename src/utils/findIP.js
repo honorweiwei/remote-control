555102ab5179de459031de7153c4ffee1571de4b
honorweiwei <honorweiwei@163.com>
2023-12-26 11:10:36 +0800

    更改获取当前网络的ip方式

--- src/utils/findIP.js
+++ src/utils/findIP.js
@@ -7,17 +7,26 @@ const networkInterfaces = os.networkInterfaces();
 // 打印所有网络接口信息
 // console.log(networkInterfaces);
 
-let localIpAddress;
 
-networkInterfaces.en0.forEach((iface) => {
-    if (iface.family === 'IPv4' && !iface.internal) {
-        localIpAddress = iface.address;
-
+// 存储满足条件的网络接口
+const connectedInterfaces = [];
+// 遍历每个网络接口
+for (const [interfaceName, interfaces] of Object.entries(networkInterfaces)) {
+    // 遍历每个接口的详细信息
+    for (const iface of interfaces) {
+        // 如果接口有IPv4地址，认为是当前连接的接口
+        if (iface.family === 'IPv4' && !iface.internal) {
+            connectedInterfaces.push({ interfaceName, iface });
+        }
     }
-});
-
-
-
-// console.log(`Local IP Address: ${localIpAddress}`);
-
-module.exports.ip = localIpAddress
+}
+// 如果有满足条件的接口，选择第一个
+if (connectedInterfaces.length > 0) {
+    const firstConnectedInterface = connectedInterfaces[0];
+    // console.log(`当前连接的网络接口是: ${firstConnectedInterface.interfaceName}`);
+    // console.log(firstConnectedInterface.iface);
+    module.exports.ip = firstConnectedInterface.iface.address
+} else {
+    console.log('没有找到满足条件的网络接口。');
+    module.exports.ip = '127.0.0.1'
+}
